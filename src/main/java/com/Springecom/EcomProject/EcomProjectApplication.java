2985343deb7ca512d786bf4120b322f895838af3
Santoshkumar Chanshetty <santoshchanshetty52@gmail.com>
2026-08-22 19:18:49 +0530

    Added application entry point comment

--- src/main/java/com/Springecom/EcomProject/EcomProjectApplication.java
+++ src/main/java/com/Springecom/EcomProject/EcomProjectApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EcomProjectApplication {
 
+	//For Learning GIT I'M ADDING THIS COMMENT
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(EcomProjectApplication.class, args);
